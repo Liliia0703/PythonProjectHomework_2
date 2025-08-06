d47c6a09648a606aa5401484b4aeaa5d4b5e5adf
Liliya <limark@MacBook-Air-Liliya.local>
2025-08-06 23:24:41 +0300

    Виконані зміни

---
+++ lesson_01/homework_1.py
@@ -0,0 +1,93 @@
+#task1
+print("Task_1")
+print("Hello", end = " ")
+print("World")
+print()
+
+#task2
+print("Task_2")
+hello = "Hello"
+world = "world"
+if True: print(f"{hello} {world}!")
+print()
+
+#task3
+print("Task_3")
+for letter in "Hello world!": print(letter)
+print()
+
+#task4
+print("Task_4")
+apples=2
+banana=apples * 4
+print(f"{apples}")
+print(f"{banana}")
+print()
+
+#task5
+print("Task_5")
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+print()
+
+#task6
+print("Task_6")
+perimetery = 1+2+3+4
+print(f"{perimetery}")
+print()
+
+#task7
+print("Task_7")
+apple_trees = 4
+pear_trees = 4 + 5
+plum_trees = 4 - 2
+together_trees = apple_trees + pear_trees + plum_trees
+print(f"Яблунь посадили {apple_trees}")
+print(f"Груш посадили на 5 більше від яблунь. Відповідь: {pear_trees}")
+print(f"Слив посадили на 2 менше. Відповідь: {plum_trees}")
+print(f"Скільки всього дерев посадили в саду? Відповідь: {together_trees} дерева")
+print()
+
+#task8
+print("Task_8")
+temperature_morning = 5
+temperature_after_lunch = temperature_morning - 10
+temperature_evening = temperature_after_lunch + 4
+print(f"До обіду температура була {temperature_morning} градусів.")
+print(f"Після обіду температура опустилася на 10 градусів. Відповідь: {temperature_after_lunch} градусів.")
+print(f"Надвечір потепліло на 4 градуси. Яка температура надвечір? Відповідь: {temperature_evening} градус.")
+print()
+
+#task9
+print("Task_9")
+boys = 24
+girls = boys // 2
+sick_boy = boys - 1
+absent_girls = girls - 2
+together = sick_boy + absent_girls
+print(f"У театральному гуртку {boys} хлопчики")
+print(f"Дівчаток в двічі менше за хлопчиків. Відповідь: {girls}")
+print(f"1 хлопчик захворів. Відповідь: {sick_boy}")
+print(f"А дві дівчинки не прийшли сьогодні. Відповідь: {absent_girls}")
+print(f"Скільки дітей у театральному гуртку сьогодні? Відповідь: {together}")
+print()
+
+#task10
+print("Task_10")
+first_book = 8
+second_book = first_book + 2
+together_first_second = first_book + second_book
+third_book = together_first_second // 2
+together_books = first_book + second_book + third_book
+print(f"Перша книга коштує {first_book} грн")
+print(f"Друга книга на 2 грн дорожча. Відповідь: {second_book} грн")
+print(f"А третя - як половина вартості першої та другої разом. Відповідь: {together_first_second} грн. Вартість третьої книги: {third_book} грн")
+print(f"Скільки будуть коштувати книги якщо купити по одному примірнику разом? Відповідь: {together_books} грн")
+
+
+
+
+
+
